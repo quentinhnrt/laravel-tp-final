28873e775147350f79dbb6581979e195e0f207b6
Quentin Honnart <quentin.honnart@gmail.com>
2024-05-29 23:49:05 +0200

    Init base routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/developers', function () {
+    return 'Developers';
+})->name('developers');
+
+Route::get('/developers/{name}', function ($name) {
+    return 'Developer: ' . $name;
+})->name('developers.detail');
+
+Route::get('/project-managers', function () {
+    return 'Project Managers';
+})->name('project-managers');
+
+Route::get('/project-managers/{name}', function ($name) {
+    return 'Project Manager: ' . $name;
+})->name('project-managers.detail');
+
+Route::prefix('administration')->name('administration.')->group(function () {
 });
