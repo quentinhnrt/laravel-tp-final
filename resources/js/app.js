8970676d2e0c64a2691ab7a9eab27589ae961262
VidarDev <86915969+VidarDev@users.noreply.github.com>
2024-05-30 22:59:40 +0200

    feat: :sparkles: intégration des transitions de page + de la 404

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+barba.use(barbaCss);
+
+barba.init({
+    transitions: [
+        {
+            name: 'with-cover',
+            to: {
+                namespace: ['with-cover'],
+            },
+            leave() {},
+            enter() {},
+        },
+    ],
+});
